7bba155d47cf41ad048777ffef0cd671a1b9e214
DAVID SHIN <kilcmn3@hotmail.com>
2019-12-15 11:23:23 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,3 +1,8 @@
 def greeting(name)
+<<<<<<< HEAD
     puts "Hello #{name.strip}. It's nice to meet you."
+=======
+  puts "Hello #{name}. It's nice to meet you.".
+
+>>>>>>> d28187d4cd086c369d0912a5dfb4783a66df25a3
 end
